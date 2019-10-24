2874c9c0bcb5175f1db9d486adecd66d9ab50eba
Kacper Piwiński <cosiekvfj@o2.pl>
2019-10-24 16:15:59 +0200

    compact

--- network.cpp
+++ network.cpp
@@ -24,27 +24,23 @@ std::string Network::login(const std::string& login, const std::string& password
     sf::Http::Response response1 = http.sendRequest(request1);
 
     // 2
-    sf::Http::Request request2("/ajax/login",
-                               sf::Http::Request::Post,
-                               "login=" + login + "&password=" + password);
+    sf::Http::Request request2("/ajax/login", sf::Http::Request::Post, "login=" + login + "&password=" + password);
     request2.setField("Cookie", response1.getField("Set-Cookie"));
     sf::Http::Response response2 = http.sendRequest(request2);
 
     // 3
     sf::Http::Request request3;
-    request3.setField("Cookie", response1.getField("Set-Cookie") + response2.getField("Set-Cookie"));
+    request3.setField("Cookie", response1.getField("Set-Cookie") + "; " + response2.getField("Set-Cookie"));
     sf::Http::Response response3 = http.sendRequest(request3);
 
     // 4
-    sf::Http::Request request4("/game/login",
-                               sf::Http::Request::Post,
-                               "id=" + get_ID(response3.getBody()));
-    request4.setField("Cookie", response1.getField("Set-Cookie") + response2.getField("Set-Cookie"));
+    sf::Http::Request request4("/game/login", sf::Http::Request::Post, "id=" + get_ID(response3.getBody()));
+    request4.setField("Cookie", response1.getField("Set-Cookie") + "; " + response2.getField("Set-Cookie"));
     sf::Http::Response response4 = http.sendRequest(request4);
 
     // 5
     sf::Http::Request request5("/game");
-    request5.setField("Cookie", response1.getField("Set-Cookie") + response2.getField("Set-Cookie"));
+    request5.setField("Cookie", response1.getField("Set-Cookie") + "; " + response2.getField("Set-Cookie"));
     sf::Http::Response response5 = http.sendRequest(request5);
 
     return get_PLAYER_TOKEN(response5.getBody());
