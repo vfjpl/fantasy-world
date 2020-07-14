8fa93e0b2fe8954170250bdac9c84bd7f224630d
Kacper Piwiński <cosiekvfj@o2.pl>
2020-07-14 20:29:55 +0200

    add tgui

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-requied libraries
+requied libraries:
 
 POCO
 https://github.com/pocoproject/poco
 
 SFML
 https://github.com/SFML/SFML
+
+TGUI
+https://github.com/texus/TGUI
--- interface.hpp
+++ interface.hpp
@@ -1,4 +1,11 @@
 #ifndef INTERFACE_HPP_INCLUDED
 #define INTERFACE_HPP_INCLUDED
 
+#include <TGUI/Gui.hpp>
+
+class Interface
+{
+    tgui::Gui gui;
+};
+
 #endif // INTERFACE_HPP_INCLUDED
